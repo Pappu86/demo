d1f011da9506f492b040db88b66078b7b8ebded1
Pappu <cse.pappu@gmail.com>
2017-05-04 02:37:45 +0600

    Add slick package in myapp.

--- packages/slick/package.js
+++ packages/slick/package.js
@@ -1,15 +1,17 @@
 Package.describe({
     name: 'pappu:slick',
     version: '1.6.0',
-    summary: 'Responsive carousel',
+    summary: 'Slick responsive carousel',
     git: '',
     documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-    api.use(['jquery','templating'], 'client');   
+Package.onUse(function (api) {
+    api.use(['jquery', 'templating'], 'client');
     api.addFiles([
         "slick.css",
+        "slick-theme.css",
+        "slick-override.css",
         "slick.js"
     ], 'client');
 });
