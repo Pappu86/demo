c99bea4ad76306fffeead099121107b9b4285ac8
Pappu <cse.pappu@gmail.com>
2017-05-12 22:25:06 +0600

    Bug fixed on slick package.

--- client/views/package/slick_package.js
+++ client/views/package/slick_package.js
@@ -1,35 +1,15 @@
 Template.slickPackage.onRendered(function () {
-    $('.slickDemo').slick({
+$(".slickDemo").slick({
         dots: true,
-        infinite: false,
-        speed: 300,
+        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+        slidesToScroll: 3
+      });
+
+$(".slickImageDemo").slick({        
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
+      });
 });
 
--- packages/slick/package.js
+++ packages/slick/package.js
@@ -2,7 +2,7 @@ Package.describe({
     name: 'pappu:slick',
     version: '1.6.0',
     summary: 'Slick responsive carousel',
-    git: '',
+    git: 'https://github.com/kenwheeler/slick.git',
     documentation: 'README.md'
 });
 
@@ -10,9 +10,8 @@ Package.onUse(function (api) {
     api.use(['jquery', 'templating'], 'client');
     api.addFiles([
         "slick.css",
-        "slick-theme.css",
-        "slick-override.css",
-        "slick.js"
+        "slick-theme.css",     
+        "slick-override.css",    
+        "slick.js",
     ], 'client');
-    api.addAssets(['slick.scss', "slick-theme.scss"], 'client');
 });
