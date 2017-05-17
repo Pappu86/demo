412c079b39242bace408db94b1819e274362a3af
pappu <cse.pappu@gmail.com>
2017-05-18 04:15:37 +0600

    show date time field.

--- client/views/package/date_time_package.js
+++ client/views/package/date_time_package.js
@@ -1,3 +1,9 @@
 Template.dateTimePackage.onRendered(function() {
    $('#datetimepicker1').datetimepicker();
+   $("#dateTime2").datetimepicker({
+        format: "dd MM yyyy - hh:ii",
+        autoclose: true,
+        todayBtn: true
+        //pickerPosition: "bottom-right"
+    });
 });
